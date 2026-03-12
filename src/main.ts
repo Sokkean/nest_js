758fc2ecc945752af8360c1ac6943f52b1f7228e
TURBOTECH\FRONT-END.13 <sounsokkean@gamil.com>
2026-03-12 17:01:46 +0700

    udpate

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DataSource } from 'typeorm';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const dataSource = app.get(DataSource);
+  if (dataSource.isInitialized) {
+    console.log('✅ Database connected successfully');
+  } else {
+    console.log('❌ Database connection failed');
+  }
+
+  await app.listen(process.env.PORT ?? 3000);
+
+  console.log(`🚀 Application running on: http://localhost:${process.env.PORT ?? 3000}`);
+}
+
+bootstrap();
